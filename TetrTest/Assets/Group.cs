b0e8056b6b460f2f370fdd6d48214cf9fc221041
SergoutbackDesktop <webart2013@gmail.com>
2021-06-08 19:23:27 +0300

    1st working version at desctop

--- TetrTest/Assets/Group.cs
+++ TetrTest/Assets/Group.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Group : MonoBehaviour
 {
     // Time since last gravity tick
-    float lastFall = 0;
+    public float lastFall = 0;
     bool isValidGridPos() 
     {        
         foreach (Transform child in transform) {
@@ -113,81 +113,7 @@ public class Group : MonoBehaviour
         foreach (Transform child in transform) {
             Vector2 v = Playfield.roundVec2(child.position);
             Playfield.grid[(int)v.x, (int)v.y] = child;
-        }
-        // Move Left
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) 
-        {
-            // Modify position
-            transform.position += new Vector3(-1, 0, 0);
-        
-            // See if it's valid
-            if (isValidGridPos())
-                // It's valid. Update grid.
-                updateGrid();
-            else
-                // Its not valid. revert.
-                transform.position += new Vector3(1, 0, 0);
-        }  
-        // Move Right
-        else if (Input.GetKeyDown(KeyCode.RightArrow)) 
-        {
-            // Modify position
-            transform.position += new Vector3(1, 0, 0);
-        
-            // See if valid
-            if (isValidGridPos())
-                // It's valid. Update grid.
-                updateGrid();
-            else
-                // It's not valid. revert.
-                transform.position += new Vector3(-1, 0, 0);
-        }      
-        // Rotate
-        else if (Input.GetKeyDown(KeyCode.UpArrow)) 
-        {
-            transform.Rotate(0, 0, -90);
-        
-            // See if valid
-            if (isValidGridPos())
-                // It's valid. Update grid.
-                updateGrid();
-            else
-                // It's not valid. revert.
-                transform.Rotate(0, 0, 90);
-        }
-
-
-        
-        // Move Downwards and Fall
-        else if (Input.GetKeyDown(KeyCode.DownArrow) ||
-                Time.time - lastFall >= 1) 
-        {   
-            // Modify position
-            transform.position += new Vector3(0, -1, 0);
-
-            // See if valid
-            if (isValidGridPos()) 
-            {
-                // It's valid. Update grid.
-                updateGrid();
-            }
-            else 
-            {
-                // It's not valid. revert.
-                transform.position += new Vector3(0, 1, 0);
-
-                // Clear filled horizontal lines
-                Playfield.deleteFullRows();
-
-                // Spawn next Group
-                FindObjectOfType<Spawner>().spawnNext();
-
-                // Disable script
-                enabled = false;
-            }
-
-            lastFall = Time.time;        
-        }
+        }        
     }
     
 }
