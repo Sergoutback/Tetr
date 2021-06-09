0903e68f3cdc9485cdc11f89544c106f2a38dbd9
SergoutbackDesktop <webart2013@gmail.com>
2021-06-10 01:43:36 +0300

    10062021_0143
    without touch

--- TetrTest/Assets/Scripts/ForMessages.cs
+++ TetrTest/Assets/Scripts/ForMessages.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ForMessages : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text messageText;    
     void Start()
     {
-        
+        StartCoroutine (ForMess ());
     }
+    IEnumerator ForMess() 
+        {                    
+        yield return new WaitForSeconds(3);
+        messageText.text = "\nPush me!";
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Message_Cliked()
+    {   
+        messageText.text = "\nHello!\nLet's play!";              
     }
 }
